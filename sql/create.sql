a06e3fdfe4dbec51a9a577505b045fe076d74fdc
Alex <if12b062@technikum-wien.at>
2014-10-20 17:03:25 +0200

    Woopsie. Pushed wrong SQL script.

--- sql/create.sql
+++ sql/create.sql
@@ -1,13 +1,145 @@
-/* CREATE DATABASE news; */
-USE news;
+CREATE DATABASE  IF NOT EXISTS `bsdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `bsdb`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: bsdb
+-- ------------------------------------------------------
+-- Server version	5.6.17-log
 
-DROP TABLE IF EXISTS t_news;
-CREATE TABLE t_news (
-  newsid INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
-  text TEXT NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO t_news (title, text) VALUES ('title1', 'text1');
-INSERT INTO t_news (title, text) VALUES ('title2', 'text2');
-INSERT INTO t_news (title, text) VALUES ('title3', 'text3');
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `idauthor` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  PRIMARY KEY (`idauthor`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Alex','Grafl','Austria','1992-05-10'),(2,'Victor','Stabrawa','Poland','1992-01-10');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book` (
+  `idbook` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `isbn` varchar(45) NOT NULL,
+  `subtitle` varchar(45) DEFAULT NULL,
+  `pubdate` date DEFAULT NULL,
+  `language` varchar(45) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `pages` int(11) DEFAULT NULL,
+  `genre` varchar(45) DEFAULT NULL,
+  `fk_publisher` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`idbook`),
+  KEY `fk_publisher_idx` (`fk_publisher`),
+  CONSTRAINT `fk_publisher` FOREIGN KEY (`fk_publisher`) REFERENCES `publisher` (`idpublisher`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'Lord of the Flies','0-571-05686-5 ','Fliiiiiiiees','1954-01-01','EN','A group of British boys stuck on an uninhabited island who try to govern themselves with disastrous results.',304,'Allegorical novel',2),(2,'The Lord of the Rings','345-24032-4','The Fellowship of the Ring','1954-07-29','EN','First part of the LotR trilogy.',432,'Fantasy',1),(3,'The Lord of the Rings','9780618002238','The Two Towers','1954-11-11','EN','Second part of the LotR trilogy.',352,'Fantasy',1),(4,'The Lord of the Rings','345-24034-0','The Return of the King','1955-10-20','EN','Third part of the LotR trilogy.',544,'Fantasy',1);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book_author`
+--
+
+DROP TABLE IF EXISTS `book_author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_author` (
+  `idbook_author` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fk_book` int(10) unsigned DEFAULT NULL,
+  `fk_author` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`idbook_author`),
+  KEY `fk_book_idx` (`fk_book`),
+  KEY `fk_author_idx` (`fk_author`),
+  CONSTRAINT `fk_author` FOREIGN KEY (`fk_author`) REFERENCES `author` (`idauthor`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_book` FOREIGN KEY (`fk_book`) REFERENCES `book` (`idbook`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_author`
+--
+
+LOCK TABLES `book_author` WRITE;
+/*!40000 ALTER TABLE `book_author` DISABLE KEYS */;
+INSERT INTO `book_author` VALUES (1,1,2),(2,2,1),(3,3,1),(4,4,1);
+/*!40000 ALTER TABLE `book_author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publisher`
+--
+
+DROP TABLE IF EXISTS `publisher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publisher` (
+  `idpublisher` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `countrycode` varchar(8) DEFAULT NULL,
+  `postcode` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`idpublisher`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publisher`
+--
+
+LOCK TABLES `publisher` WRITE;
+/*!40000 ALTER TABLE `publisher` DISABLE KEYS */;
+INSERT INTO `publisher` VALUES (1,'GRAFL Ltd.','AT','1220'),(2,'Polska Mafia','PL','1234');
+/*!40000 ALTER TABLE `publisher` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-10-20 17:02:33
